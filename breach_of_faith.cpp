4863b915ee8484720d3084a67a24536d4c0e56c2
Jattzz <108416182+jattzz@users.noreply.github.com>
2025-08-04 23:43:08 +0530

    Create breach_of_faith.cpp

---
+++ breach_of_faith.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define vi vector<int> 
+#define vll vector<long long>
+#define pii pair<int, int> 
+#define pic pair<int, char> 
+#define mii map<int, int> 
+#define mll map<long long, long long>
+#define pb push_back
+#define mp make_pair
+#define sortv(v) sort(v.begin(), v.end())
+#define sumup(v) accumulate(v.begin(), v.end(), 0)
+#define ull unsigned long long int
+#define bs binary_search
+#define lb lower_bound
+#define ub upper_bound
+#define cinv for(auto &i: v) cin>>i
+ll power(ll x, ull y){
+    if(y==0){
+        return 1;
+    }
+    ll temp = power(x, y/2);
+    if(y%2==0){
+        return temp*temp;
+    }
+    else{
+        return x*temp*temp;
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t; cin>>t;
+    while(t--){
+        ll n; cin>>n;
+        vll v(n);
+        for(auto &i: v) cin>>i;
+        ll mx = *max_element(v.begin(), v.end());
+        sort(v.begin(), v.end());
+        cout<<mx+1<<" ";
+        for(auto i: v) cout<<i<<" ";
+        cout<<endl;
+    }
+}
