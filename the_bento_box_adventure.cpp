5c7c8981b1c5cb72d96e63097d8645aa05cb3723
Jattzz <108416182+jattzz@users.noreply.github.com>
2025-08-13 20:08:46 +0530

    Create the_bento_box_adventure.cpp

---
+++ the_bento_box_adventure.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define vi vector<int> 
+#define vll vector<long long>
+#define pii pair<int, int> 
+#define pic pair<int, char> 
+#define mii map<int, int> 
+#define mll map<long long, long long>
+#define pb push_back
+#define mp make_pair
+#define sortv(v) sort(v.begin(), v.end())
+#define sumup(v) accumulate(v.begin(), v.end(), 0)
+#define ull unsigned long long int
+#define bs binary_search
+#define lb lower_bound
+#define ub upper_bound
+#define cinv for(auto &i: v) cin>>i
+ll power(ll x, ull y){
+    if(y==0){
+        return 1;
+    }
+    ll temp = power(x, y/2);
+    if(y%2==0){
+        return temp*temp;
+    }
+    else{
+        return x*temp*temp;
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int sum = 15;
+    for(int i = 0; i<4; i++){
+        int x; cin>>x;
+        sum -= x;
+    }
+    cout<<sum<<endl;
+}
+
+// ICPC problem btw, should be rated 50 instead of 1300
