7e6dffc84c69ef2ded999a17efdca31f79ae184f
Jattzz <108416182+jattzz@users.noreply.github.com>
2025-08-14 20:09:25 +0530

    Create little_elephant_and_problem.cpp

---
+++ little_elephant_and_problem.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define vi vector<int> 
+#define vll vector<long long>
+#define pii pair<int, int> 
+#define pic pair<int, char> 
+#define mii map<int, int> 
+#define mll map<long long, long long>
+#define pb push_back
+#define mp make_pair
+#define sortv(v) sort(v.begin(), v.end())
+#define sumup(v) accumulate(v.begin(), v.end(), 0)
+#define ull unsigned long long int
+#define bs binary_search
+#define lb lower_bound
+#define ub upper_bound
+#define cinv for(auto &i: v) cin>>i
+ll power(ll x, ull y){
+    if(y==0){
+        return 1;
+    }
+    ll temp = power(x, y/2);
+    if(y%2==0){
+        return temp*temp;
+    }
+    else{
+        return x*temp*temp;
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll n; cin>>n;
+    vll v(n);
+    for(auto &i: v) cin>>i;
+    vll copy = v;
+    sort(copy.begin(), copy.end());
+    ll cnt = 0;
+    for(int i = 0; i<n; i++){
+        cnt += (v[i]!=copy[i]);
+    }
+    if(cnt<=2) cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+}
+
+// pretty standard problem
